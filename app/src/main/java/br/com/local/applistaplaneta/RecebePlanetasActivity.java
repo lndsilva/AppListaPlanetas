76fd6133b5c6935c6dd4a30a8b948bff533cb309
lndsilva <lndsilva@hotmail.com>
2022-07-22 15:26:40 -0300

    criando lista e clique

---
+++ app/src/main/java/br/com/local/applistaplaneta/RecebePlanetasActivity.java
@@ -0,0 +1,54 @@
+package br.com.local.applistaplaneta;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RecebePlanetasActivity extends AppCompatActivity {
+
+    TextView txtRecebeNomePlanetas;
+    ImageView imgRecebePlanetas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recebe_planetas_layout);
+
+        txtRecebeNomePlanetas = findViewById(R.id.txtRecebeNomePlaneta);
+        imgRecebePlanetas = findViewById(R.id.imgRecebePlaneta);
+
+        //Intent que receberá os dados da ourta activity
+        Intent intent = getIntent();
+
+        //Declarar as variáveis para receber os valores do mesmo tipo
+
+        String nome = intent.getStringExtra("nome_planeta");
+        int imagem = intent.getIntExtra("imagem_planeta", 0);
+
+        //os objetos recebendo os valores
+        txtRecebeNomePlanetas.setText(nome);
+        imgRecebePlanetas.setImageResource(imagem);
+
+        //inserindo a seta na appBar da activity
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
